ff835a9f38ddc2285fa70022d0e7023025ff3f00
Sei2772 <authanapat2703@gmail.com>
2025-05-07 00:02:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,15 +23,15 @@ app.get('/', function (req, res) {
 
 
 
-// const dbcon = mysql.createConnection({ 
-//     host: process.env.DB_HOST || 'localhost', 
-//     user: process.env.DB_USER || 'root', 
-//     password: process.env.DB_PASSWORD || '', 
-//     database: process.env.DB_NAME || 'mydb' });
+const dbcon = mysql.createConnection({ 
+    host: process.env.DB_HOST || 'localhost', 
+    user: process.env.DB_USER || 'root', 
+    password: process.env.DB_PASSWORD || '', 
+    database: process.env.DB_NAME || 'mydb' });
 
 
 
-// dbcon.connect();
+dbcon.connect();
 
 
 
